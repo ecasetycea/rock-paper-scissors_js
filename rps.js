fcac64f7e973208e4df60b4bab8be457a1bc5bdc
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 12:11:47 +0300

    Add computer choice func

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-alert("Hello html");
+function getComputerChoice() {
+    pick = Math.floor(Math.random()*3);
+
+    switch(pick) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
